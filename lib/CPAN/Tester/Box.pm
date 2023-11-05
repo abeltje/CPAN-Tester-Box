020a0a199673ea3362ca6ecdef97ba53bd4a66a7
Abe Timmerman <abeltje@cpan.org>
2023-11-05 15:47:42 +0100

    Autocommit for distribution CPAN-Tester-Box 0.01_03 (test)

--- lib/CPAN/Tester/Box.pm
+++ lib/CPAN/Tester/Box.pm
@@ -4,7 +4,7 @@ use v5.20.0;
 use if $] <  5.036, experimental => 'signatures';
 use if $] >= 5.036, feature      => 'signatures';
 
-our $VERSION = "0.01_02";
+our $VERSION = "0.01_03";
 
 use File::Spec::Functions qw< devnull >;
 use DateTime;
